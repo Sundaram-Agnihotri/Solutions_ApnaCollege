fbff5b6c75bb1a338d5372bb221729de1b6c005c
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-09-07 21:19:25 +0530

    Create Reverse a String.cpp

---
+++ Reverse a String.cpp
@@ -0,0 +1,16 @@
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+       int n = str.size();
+        for (int i = 0; i < n/2; i++){
+            char temp = str[i];
+            str[i] = str[n-1-i];
+            str[n-i-1] = temp;
+        }
+        return str;
+    }
+};
